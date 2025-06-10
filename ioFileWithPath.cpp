ea7ff5f4ab7e8d96ac79c0292c94e397b7ac8204
upi1682 <upi538287@gmail.com>
2025-06-10 15:23:10 +0700

    membuat fungsi

---
+++ ioFileWithPath.cpp
@@ -0,0 +1,26 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string baris;
+    string namaFile;
+
+    cout << "masukan nama file: ";
+    cin >> namaFile;
+
+
+   
+
+  
+
+  
+ 
+
+ 
+
+
+  
+    
+}
