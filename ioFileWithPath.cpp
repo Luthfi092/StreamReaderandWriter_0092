10ca4e614a36cb532bde4aae64edf49c3ce728d0
upi1682 <upi538287@gmail.com>
2025-06-10 15:25:58 +0700

    melakukan perulangan

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -36,7 +36,18 @@ int main(){
     infile.open(namaFile +".txt");
 
     cout << endl << ">= Membuka dan membaca file" << endl;
-
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        //melakukan perulangan setiap baris
+        while (getline(infile,baris))
+        {
+            //dan tampilka disini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
   
     
 }
